90d0f11aa3c0aa512c488bbe22d4872eece85fc3
icinisikim <109095013+icinisikim@users.noreply.github.com>
2022-09-27 15:01:47 +0400

    Add files via upload

---
+++ mainPage.js
@@ -0,0 +1,34 @@
+
+function dark(){
+    var lightButton = document.getElementById("lighti");
+    var darkButton = document.getElementById("darki");
+    document.body.style.backgroundColor = "#15151e";
+    document.body.style.color = "white";
+    document.getElementById("nav-menu").style.backgroundColor = "#2B2B3B";
+    document.getElementById("name").style.color = "white";
+    // document.getElementById("about").style.color = "white";
+    document.getElementById("exit").style.color = "white";
+    document.getElementById("reset").style.color = "white";
+    document.getElementById("comp").style.color = "white";
+    document.getElementById("start-shp").style.color = "white";
+    document.getElementById("slogan").style.color = "white";
+    document.getElementById("get-started").style.color = "white";
+    document.getElementById("top").style.color = "white";
+    document.getElementById("brand-ch").style.color = "white";
+}
+function light(){
+    var lightButton = document.getElementById("lighti");
+    var darkButton = document.getElementById("darki");
+    document.body.style.backgroundColor = "#ffffff";
+    document.getElementById("nav-menu").style.backgroundColor = "#a8b1b3";
+    document.getElementById("name").style.color = "black";
+    // document.getElementById("about").style.color = "black";
+    document.getElementById("exit").style.color = "black";
+    document.getElementById("reset").style.color = "black";
+    document.getElementById("comp").style.color = "black";
+    document.getElementById("start-shp").style.color = "black";
+    document.getElementById("slogan").style.color = "black";
+    document.getElementById("get-started").style.color = "black";
+    document.getElementById("top").style.color = "black";
+    document.getElementById("brand-ch").style.color = "black";
+}
